ba121741b4359b9a685f9775580b4c8442ed52c7
MathItYT <benjamin.ubilla@uc.cl>
2026-03-30 14:00:03 -0300

    Fix MaskMobject, modificar README.md y ejemplos

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -58,6 +58,7 @@ from manimlib.mobject.svg.text_mobject import *
 from manimlib.mobject.three_dimensions import *
 from manimlib.mobject.types.dot_cloud import *
 from manimlib.mobject.types.image_mobject import *
+from manimlib.mobject.types.mask_mobject import *
 from manimlib.mobject.types.point_cloud_mobject import *
 from manimlib.mobject.types.surface import *
 from manimlib.mobject.types.vectorized_mobject import *
